1896d931c4a74bb319b22c121df34151a5d15d39
Trung <namtrung18101996@gmail.com>
2018-02-22 21:56:02 +0700

    Bai 7 Angular2 căn bản

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template: '<h4>Học hutech</h4>',
+  styles: ['h4 {color:blue}'],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  title = 'TRUNG';
+  //property binding
+  public image = 'http://media.tinthethao.com.vn/files/bongda/2017/09/27/nhung-ban-thang-mang-thuong-hieu-david-villa-215826jpg.jpg';
+
+  public welcome = 'Anh Trung đẹp trai';
 }
---
+++ src/app/tutorial.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // tslint:disable-next-line:component-selector
+    selector: 'my-tutorial',
+    template: `<h2>Bùi Nam Trung</h2>
+    <h4>Học hutech</h4>
+    `,
+    // styleUrls: ['./app.component.css'],
+    styles: ['h2 {color:red}', 'h4 {color:blue}'],
+})
+export class TutorialComponent {
+
+}
