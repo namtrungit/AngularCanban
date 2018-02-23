baed8026b1fe9b69f70f9e9f1dcbf90a45d02df1
Trung <namtrung18101996@gmail.com>
2018-02-23 23:15:59 +0700

    Lesson15:  Xây dựng form từ template, sử dụng ngModel, ngModelGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Hello TRUNG'; // uppercase vs lowercase
-  today = Date.now(); // date pipe
-  // percent pipe
-  percentA = 0.269;
-  percentB = 1.3495;
-  // Decimal Pipe
-  numberA = 3.14;
-  numberB = 2.1234567;
-  // Slice pipe (cắt mảng)
-  arrStudent = ['Trung', 'Tuấn', 'Vỹ', 'Thịnh'];
   // property binding
-  public image = 'http://media.tinthethao.com.vn/files/bongda/2017/09/27/nhung-ban-thang-mang-thuong-hieu-david-villa-215826jpg.jpg';
-
+  public title = 'Trung';
+  onSubmit(value: any) {
+    console.log(value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TutorialComponent } from './tutorial.component';
 import { FormsModule } from '@angular/forms'; // Thư viện chứa ngModel
-import {ExponentialStrengthPipe} from './exponential-strength.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent, TutorialComponent, ExponentialStrengthPipe
+    AppComponent, TutorialComponent
   ],
   imports: [
     BrowserModule, FormsModule
