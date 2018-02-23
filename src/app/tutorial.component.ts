8eee8426d24e387b6373ff094e09935539a74387
Trung <namtrung18101996@gmail.com>
2018-02-23 18:26:32 +0700

    Lesson 11:  Stuctural directives(ngFor ,ngIf, ngSwitch)

--- src/app/tutorial.component.ts
+++ src/app/tutorial.component.ts
@@ -3,18 +3,22 @@ import { Component } from '@angular/core';
 @Component({
     // tslint:disable-next-line:component-selector
     selector: 'my-tutorial',
-    template: `<h2>{{title}}</h2>
-    <input type="text" [(ngModel)] = "fname"/>
-    <input type="text" [(ngModel)] = "lname"/>
-    <br/>
-    full name: {{lname}} {{fname}}
+    template: `<h3 *ngIf="showLineIf">this ngIf directive line.</h3>
+    <div [ngSwitch] = "color">
+        <p *ngSwitchCase="'red'">this is color is red</p>
+        <p *ngSwitchCase="'blue'">this is color is blue</p>
+        <p *ngSwitchCase="'green'">this is color is green</p>
+        <p *ngSwitchDefault>invalid color</p>
+    </div>
+    <ul>
+        <li *ngFor = "let cl of colors">
+            {{cl}}
+        </li>
+    </ul>
     `,
-    // styleUrls: ['./app.component.css'],
-    styles: [`.greenColor{
-        color:green;
-    }`],
 })
 export class TutorialComponent {
-    public title = 'Bùi Nam Trung';
-
+    public showLineIf = true; // *ngIf True thì hiển thị thẻ h3 và ngược lại
+    public color = 'abc'; // [ngSwitch]
+    public colors: string[] = ['red', 'blue', 'green']; // *ngFor
 }
