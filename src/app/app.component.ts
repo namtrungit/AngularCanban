2faee2a6ed511f616a75949428739085a2f4f7da
Trung <namtrung18101996@gmail.com>
2018-02-24 11:33:45 +0700

    lesson16: Validate Form P1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // template: '<h4>Học hutech</h4>',
-  styles: ['h4 {color:blue}'],
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
